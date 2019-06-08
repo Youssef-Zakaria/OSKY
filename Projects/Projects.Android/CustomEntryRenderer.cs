6a7fb1e23e6a4107012c5752a137e83656edca10
eng.kerolostalat <eng.kerolostalat@gmail.com>
2019-06-08 13:23:06 +0200

    Login

---
+++ Projects/Projects.Android/CustomEntryRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using Projects.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamarinEntry.Droid;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace XamarinEntry.Droid
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+            AutoPackage = false;
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Background = new ColorDrawable(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
